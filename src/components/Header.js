75349c2f3e1b3b972c95d4685d550968ea7d40ad
Cody Cooper <cody_codster@live.com>
2023-03-09 04:53:12 -0600

    Added additional pages and lifted up to parent container component. Adjusted css for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,31 @@ export default function Header({ currentPage, handlePageChange }) {
     return (
         <div className="header">
             <ul>
-                <li>HOME</li>
-                <li>ABOUT</li>
-                <li>PROJECTS</li>
-                <li>RESUME</li>
-                <li>CONTACT</li>
+                <li
+                className={ currentPage === 'About' ? 'activeNav' : 'lazyNav' }
+                onClick={() => handlePageChange('About')}
+                 >ABOUT</li>
+
+                <li
+                className={ currentPage === 'Projects' ? 'activeNav' : 'lazyNav' }
+                onClick={() => handlePageChange('Projects')}
+                >PROJECTS</li>
+
+                <li 
+                className={ currentPage === 'Home' ? 'activeNav' : 'lazyNav' }
+                onClick={() => handlePageChange('Home')}
+                >HOME</li>
+                
+                <li 
+                className={ currentPage === 'Resume' ? 'activeNav' : 'lazyNav' }
+                onClick={() => handlePageChange('Resume')}
+                >RESUME</li>
+
+                <li 
+                className={ currentPage === 'Contact' ? 'activeNav' : 'lazyNav' }
+                onClick={() => handlePageChange('Contact')}
+                >CONTACT</li>
             </ul>
         </div>
-    )
-}
+    );
+};
--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,10 +1,34 @@
+// import userEvent from "@testing-library/user-event";
 import React from "react";
+import { useState } from "react";
 import Header from './Header';
+import Footer from "./Footer";
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
 
 export default function PageContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'Home': return <Home></Home>;
+            case 'About': return <About></About>;
+            case 'Projects': return <Projects></Projects>;
+            case 'Resume': return <Resume></Resume>;
+            case 'Contact': return <Contact></Contact>;
+            default: return <Home></Home>;
+        };
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
     return (
         <div className="pageContainer">
-            <Header></Header>
+            <Header currentPage={currentPage} handlePageChange={handlePageChange}></Header>
+            {renderPage()}
+            <Footer></Footer>
         </div>
-    )
-}
+    );
+};
