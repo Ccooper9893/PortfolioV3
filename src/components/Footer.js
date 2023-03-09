ee1851790c1bcd647f00b248acb862ef679001fc
Cody Cooper <cody_codster@live.com>
2023-03-09 07:36:42 -0600

    Imported icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+// import githubLogoH from '../assets/icons/GithubLogowhite.png';
+// import githubLogo from '../assets/icons/GithubLogo.png';
+// import linkedinLogoH from '../assets/icons/linkedinWhite.png';
+// import linkedinLogo from '../assets/icons/linkedinBlack.png';
+// import '../styles/Footer.css';
 
 export default function Footer() {
+    // const [isHovering, setIsHovering] = useState(false);
+    // const [isHovering2, setIsHovering2] = useState(false);
     // return (
     //     <div className="footer">
-    //         <ul>
-    //         </ul>
+    //       <a href='https://github.com/Ccooper9893' target='_blank'>
+    //         <img
+    //             src={isHovering ? githubLogoH : githubLogo}
+    //             alt="Github logo"
+    //             onMouseOver={() => setIsHovering(true)}
+    //             onMouseOut={() => setIsHovering(false)}
+    //         />
+    //         </a>
+    //         <a href='https://github.com/Ccooper9893' target='_blank'>
+    //         <img
+    //             src={isHovering2 ? linkedinLogoH : linkedinLogo}
+    //             alt="Github logo"
+    //             onMouseOver={() => setIsHovering2(true)}
+    //             onMouseOut={() => setIsHovering2(false)}
+    //         />
+    //         </a>
     //     </div>
-    // )
+    //   );
 };
+
+
+
