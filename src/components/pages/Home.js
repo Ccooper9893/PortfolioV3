dd13a7d33d77eaad2f42926422b4971abdb18042
Cody Cooper <cody_codster@live.com>
2023-03-10 02:03:53 -0600

    Messing around with home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import selfie from '../../assets/images/selfie1.png'
+import '../styles/Home.css'
 
 export default function Home() {
     return (
-        <h1>Home Page</h1>
+        <div className='main'>
+            <div className='selfie'>
+                <img src={selfie} alt='Cody Cooper'></img>
+            </div>
+        </div>
     )
 };
