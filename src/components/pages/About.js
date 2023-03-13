18c478930299fbe9677a78cf646773b0fe9b9cc3
Cody Cooper <cody_codster@live.com>
2023-03-13 18:49:41 -0500

    Polishing About page 1

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -15,47 +15,45 @@ export default function About() {
     return (
         <div className="h-96 carousel carousel-vertical rounded-box">
 
-        <div className="carousel-item h-full flex-col mt-20">
+        <div className="carousel-item h-full flex-col mt-20 text-center">
 
-        <motion.h1 animate={{ y: 0, opacity: 1 }} initial={{ y: 0, opacity: 0 }} transition={{ delay: 0.3 }} className="text-3xl lg:text-5xl text-blue-400 text-center lg:mb-10 lg:pt-10 lg:text-6xl">ABOUT</motion.h1>
+        <h1 className="text-lg lg:text-4xl lg:mt-10 text-blue-400">ABOUT ME</h1>
 
-            <div className="grid lg:grid-cols-3 gap-10 mx-4 border-t-yellow-500 ">
+            <div className="grid lg:grid-cols-3 gap-4 mx-4 border-t-yellow-500 ">
 
-                <div className="text-center mt-3">
-                    <h3 className="text-yellow-500 lg:text-3xl">GOALS</h3>
-                    <motion.p className="lg:text-2xl md:text-lg sm:text-sm mt-2 text-center" animate={{ y: 0, opacity: 1 }} initial={{ y: 0, opacity: 0 }} transition={{ type:'tween', ease: "easeInOut", delay: 1 }}>
+                <motion.div className=" mt-3" animate={{ y: 0, opacity: 1 }} initial={{ y: '100vh', opacity: 0 }} transition={{ type:"just", ease: "easeIn", delay: 1 }}>
+                <h3 className="text-yellow-500 lg:text-2xl shadow-sm shadow-yellow-500">GOALS</h3>
+                    <p className="text-sm md:text-lg">
 
                     My goal is to work with a team to develop dynamic and responsive web applications that can make an impact in the tech industry. 
                     I am committed to pursuing opportunities that challenge and push me to grow both personally and professionally as a web developer.
-                    </motion.p>
-                </div>
+                    </p>
+                </motion.div>
 
-                <div class="text-center">
-                    <motion.div className="flex relative justify-center mb-14" animate={{ y: 0, opacity: 0.7 }} initial={{ y: 0, opacity: 0 }} transition={{ ease: "easeInOut", delay: 2.8 }}>
-                            <img src={bigGear} className="w-20 animate-spin rotate-180" alt="Big gear" style={{animationDuration: '15s'}}></img>
-                            <img src={mdGear} className="w-20 animate-spin rotate-180" alt="medium gear" style={{animationDuration: '15s'}}></img>
-                            <img src={smGear} className="absolute w-20 animate-spin" alt="Small gear" style={{animationDuration: '15s', marginLeft: '15px', top: '36px'}}></img>
-                        </motion.div>
-                    <h3 className="text-yellow-500 lg:text-3xl">DRIVE</h3>
-                    <motion.p className="lg:text-2xl md:text-lg sm:text-sm text-center" animate={{ y: 0, opacity: 1 }} initial={{ y: 0, opacity: 0 }} transition={{ type:'spring', ease: "easeInOut", delay: 1.5 }}>
-                    
-
+                <motion.div className="text-center lg:mt-20" animate={{ y: 0, opacity: 1 }} initial={{ y: '100vh', opacity: 0 }} transition={{ ease: "easeInOut", delay: 0.5 }}>
+                    <div className="flex relative justify-center mb-14 opacity-80">
+                            <img src={bigGear} className="w-10 lg:w-20 animate-spin rotate-180" alt="Big gear" style={{animationDuration: '15s'}}></img>
+                            <img src={mdGear} className="w-10 lg:w-20 animate-spin rotate-180" alt="medium gear" style={{animationDuration: '15s'}}></img>
+                            <img src={smGear} className="absolute lg:w-20 w-10 animate-spin" alt="Small gear" style={{animationDuration: '15s', marginLeft: '15px', top: '36px'}}></img>
+                    </div>
+                    <motion.h3 className="text-yellow-500 lg:text-2xl shadow-sm shadow-yellow-500">DRIVE</motion.h3>
+                    <motion.p className="text-sm md:text-lg">
                         My passion lies in building dynamic, user-friendly web applications that solve real-world problems. 
                         I am eager to continue growing as a developer and to contribute to innovative solutions that push the boundaries of what is possible in web development. 
                     </motion.p>
-                </div>
+                </motion.div>
 
-                <div className="text-center">
-                    <h3 className="text-yellow-500 lg:text-3xl">EXPERIENCE</h3>
-                    <motion.p className="lg:text-2xl md:text-lg sm:text-sm mt-2 text-center" animate={{ y: 0, opacity: 1 }} initial={{ y: 0, opacity: 0 }} transition={{ type:'spring', ease: "easeInOut", delay: 2 }}>
+                <motion.div className="text-center"animate={{ y: 0, opacity: 1 }} initial={{ y: '100vh', opacity: 0 }} transition={{ type:"just", ease: "easeIn", delay: 1.5 }}>
+                <h3 className="text-yellow-500 lg:text-2xl shadow-sm shadow-yellow-500">EXPERIENCE</h3>
+                    <motion.p className="text-sm md:text-lg">
                         My background in customer service and team leadership roles has given me the ability to communicate effectively and work collaboratively. Having led a team of over 40 employees, I understand the importance of effective communication.
                     </motion.p>
-                </div>
+                </motion.div>
           </div>
 
-                <motion.div className="animate-bounce text-center mt-14" animate={{ y: 0, opacity: 1 }} initial={{ y: 0, opacity: 0 }} transition={{ ease: "easeInOut", delay: 5 }}>
-                    <p className="text-white">Scroll down to continue</p>
-                    <p className="text-white">V</p>
+                <motion.div className="animate-bounce text-center mt-8 lg:mt-40" animate={{ y: 0, opacity: 1 }} initial={{ y: 0, opacity: 0 }} transition={{ ease: "easeInOut", delay: 4 }}>
+                    <p className="text-white opacity-50">Scroll down to continue</p>
+                    <p className="text-white opacity-50">V</p>
                 </motion.div>
         </div> 
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,17 +12,17 @@ export default function Home({handlePageChange}) {
           
         <div className="hero-content p-0 flex-col-reverse lg:flex-row-reverse">
 
-          <motion.img className="fade-in-4" animate={{ x: 0, opacity: 1 }} initial={{ x: 0, opacity: 0 }} transition={{ delay: 3.5 }} src={selfie} alt="Cody Cooper"/>
+          <motion.img className="fade-in-4" animate={{ x: 0, opacity: 1 }} initial={{ x: '100vh', opacity: 0 }} transition={{ delay: 3 }} src={selfie} alt="Cody Cooper"/>
           <div className="text-center">
 
-            <motion.h1 className="text-7xl mt-5 text-white" animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 0.5 }}>WELCOME!</motion.h1>
+            <motion.h1 className="text-7xl mt-5 text-white" animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 0.3 }}>WELCOME!</motion.h1>
 
-            <motion.p animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 1.6 }} className="py-3 text-4xl">MY NAME IS
-              <motion.span animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 2.4 }} className="text-yellow-500"> CODY COOPER</motion.span>
+            <motion.p animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 1 }} className="py-3 text-4xl">MY NAME IS
+              <motion.span animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 1.6 }} className="text-yellow-500"> CODY COOPER</motion.span>
             </motion.p>
 
-            <motion.p animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 5 }} className="text-2xl">I AM A 
-              <motion.span animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 5.4 }} className="text-blue-400"> FULL STACK WEB DEVELOPER</motion.span>
+            <motion.p animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 4.5 }} className="text-2xl">I AM A 
+              <motion.span animate={{ x: 0, opacity: 1 }} initial={{ x: '0', opacity: 0 }} transition={{ delay: 5 }} className="text-blue-400"> FULL STACK WEB DEVELOPER</motion.span>
             </motion.p>
             
 
@@ -32,7 +32,7 @@ export default function Home({handlePageChange}) {
               <motion.img className='w-12 hover:animate-pulse' alt="React" src={reactIcon.toString()} animate={{ x: 0, opacity: 1 }} initial={{ x: -100, opacity: 0 }} transition={{ delay: 6.4 }} />
               <motion.img className='w-12 hover:animate-pulse' alt="Node" src={nodeIcon.toString()} animate={{ x: 0, opacity: 1 }} initial={{ x: -100, opacity: 0 }} transition={{ delay: 6.2 }} />
             </div>
-            <motion.button className='text-lg animate-pulse p-0 mt-3 hover:text-yellow-500' onClick={() => handlePageChange('About')} animate={{ x: 0, opacity: 1 }} initial={{ x: -100, opacity: 0 }} transition={{ delay: 8 }}>Continue &#x2192;</motion.button>
+            <motion.button className='text-lg animate-pulse p-0 mt-3 hover:font-bold' onClick={() => handlePageChange('About')} animate={{ x: 0, opacity: 1 }} initial={{ x: -100, opacity: 0 }} transition={{ delay: 8 }}>Continue &#x2192;</motion.button>
             
           </div>
         </div>
