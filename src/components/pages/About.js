01fd4404b0cfd508888b443286a775affb3d9c49
Cody Cooper <cody_codster@live.com>
2023-03-14 19:25:15 -0500

    Updated skills page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,106 +9,115 @@ import lightbulb from '../../assets/icons/lightbulb.png';
 
 export default function About({ handlePageChange }) {
     return (
-          <div className="h-full flex-col text-center about">
-              <div className="grid lg:grid-cols-2 gap-4 lg:gap-20 mx-10 mt-12">
-              <motion.div 
-              className="shadow-sm hover:shadow-blue-500 p-4"
-                  animate={{ x: 0, opacity: 1 }} 
-                  initial={{ x: '-100vw', opacity: 0 }} 
-                  transition={{ type: 'tween', ease: "easeIn", delay: 0.5 }}
-                >
-                <div className="flex relative justify-center mb-14 opacity-90">
-                    <motion.img 
-                      src={bigGear} 
-                      className="w-20 lg:w-24 animate-spin rotate-180" 
-                      alt="Big gear" 
-                      style={{animationDuration: '15s'}}
-                    >
-                    </motion.img>
-                    <img 
-                      src={mdGear} 
-                      className="w-20 lg:w-24 animate-spin rotate-180" 
-                      alt="medium gear" 
-                      style={{animationDuration: '15s'}}
-                    >
-                    </img>
-                    <img 
-                      src={smGear} 
-                      className="absolute lg:w-24 w-20 animate-spin" 
-                      alt="Small gear" 
-                      style={{animationDuration: '15s', marginLeft: '15px', top: '36px'}}
-                      >
-                    </img>
-                  </div>
-                  <motion.h3 className="text-yellow-500 lg:text-2xl shadow-sm">DRIVE</motion.h3>
-                  <motion.p className="text-lg lg:text-2xl mx-8">
-                      My passion lies in building dynamic, user-friendly web applications that solve real-world problems. 
-                      I am eager to continue growing as a developer and to contribute to innovative solutions that push the boundaries of what is possible in web development.
-                  </motion.p>
-                </motion.div>
-
-                <motion.div  
-                className="shadow-sm hover:shadow-blue-500 p-4"
-                  animate={{ x: 0, opacity: 1 }} 
-                  initial={{ x: '100vh', opacity: 0 }} 
-                  transition={{ 
-                    type:"tween", 
-                    ease: "easeIn", 
-                    delay: 1 
-                  }}
-                >
-                  <div className="flex justify-center mt-2">
-                    <img className="flex justify-center opacity-90 w-28 mb-8" src={target} alt="arrow target">
-                    </img>
-                  </div>
-                  <h3 className="text-yellow-500 lg:text-2xl shadow-sm">GOALS</h3>
-                  <p className="text-lg lg:text-2xl mx-8">
-                    My goal is to work with a team to develop dynamic and responsive web applications that can make an impact in the tech industry. 
-                    I am committed to pursuing opportunities that challenge and push me to grow both personally and professionally as a web developer.
-                  </p>
-                </motion.div>
-
-                <motion.div 
-                  className="text-center mt-7 shadow-sm hover:shadow-blue-500 p-4"
-                  animate={{ x: 0, opacity: 1 }} 
-                  initial={{ x: '-100vw', opacity: 0 }} 
-                  transition={{ type:"tween", ease: "easeIn", delay: 1.5 }}
-                >
-                  <div className="flex justify-center mb-7 mt-2">
-                    <img className="opacity-90 w-28" src={briefcase} alt="briefcase">
-                      </img>
-                  </div>
-                  <h3 className="text-yellow-500 lg:text-2xl shadow-sm mt-6">EXPERIENCE</h3>
-                    <motion.p className="text-lg lg:text-2xl mx-8">
-                        My background in customer service and team leadership roles has given me the ability to communicate effectively and work collaboratively. Having built and led a team of over 40 employees, I understand the importance of effective communication and respectfulness.
-                    </motion.p>
-                </motion.div>
-
-                <motion.div 
-                  className="shadow-sm hover:shadow-blue-500 p-4"
-                  animate={{ x: 0, opacity: 1 }} initial={{ x: '100vh', opacity: 0 }} 
-                  transition={{ type:"tween", ease: "easeIn", delay: 1.8 }}
-                >
-                  <div className="flex justify-center">
-                    <img className="flex justify-center opacity-90 mb-6 w-36 animate-pulse" src={lightbulb} alt="lightbulb">
-                      </img>
-                  </div>
-                  <h3 className="text-yellow-500 lg:text-2xl shadow-sm">TRAITS</h3>
-                    <motion.p className="text-lg lg:text-2xl">
-                    I'm a hardworking and respectful person who values learning from others. I enjoy woodworking and coding as hobbies, and I believe in the importance of listening to others to continue growing both personally and professionally.
-                    </motion.p>
-                </motion.div>
-
-              </div>
-              <motion.button 
-                className='text-2xl lg:text-4xl animate-pulse p-0 mt-20 mb-10 hover:text-amber-500' 
-                onClick={() => handlePageChange('Skills')} 
-                animate={{ x: 0, opacity: 1 }} 
-                initial={{ x: -100, opacity: 0 }} 
-                transition={{ delay: 8 }}
+          <div className="h-full flex-col text-center about mt-3">
+            <motion.h1 
+              className="text-6xl text-blue-400"animate={{ opacity: 1 }} 
+              initial={{ opacity: 0 }} 
+              transition={{ delay: 0.2 }}
               >
-                Continue &#x2192;
-              </motion.button>
+                ABOUT ME
+            </motion.h1>
+            <div className="grid lg:grid-cols-2 mt-4">
+
+
+            <motion.div 
+            className="p-4"
+                animate={{ x: 0, opacity: 1 }} 
+                initial={{ x: '-100vw', opacity: 0 }} 
+                transition={{ type: 'tween', ease: "easeIn", delay: 0.2 }}
+              >
+              <div className="flex relative justify-center mb-14 opacity-90">
+                  <motion.img 
+                    src={bigGear} 
+                    className="w-20 lg:w-24 animate-spin rotate-180" 
+                    alt="Big gear" 
+                    style={{animationDuration: '15s'}}
+                  >
+                  </motion.img>
+                  <img 
+                    src={mdGear} 
+                    className="w-20 lg:w-24 animate-spin rotate-180" 
+                    alt="medium gear" 
+                    style={{animationDuration: '15s'}}
+                  >
+                  </img>
+                  <img 
+                    src={smGear} 
+                    className="absolute lg:w-24 w-20 animate-spin" 
+                    alt="Small gear" 
+                    style={{animationDuration: '15s', marginLeft: '15px', top: '36px'}}
+                    >
+                  </img>
+                </div>
+                <motion.h3 className="text-yellow-500 lg:text-2xl shadow-sm">DRIVE</motion.h3>
+                <motion.p className="text-lg lg:text-xl mx-8">
+                    My passion lies in building dynamic, user-friendly web applications that solve real-world problems. 
+                    I am eager to continue growing as a developer and to contribute to innovative solutions that push the boundaries of what is possible in web development.
+                </motion.p>
+              </motion.div>
+
+              <motion.div  
+              className="p-4"
+                animate={{ x: 0, opacity: 1 }} 
+                initial={{ x: '100vh', opacity: 0 }} 
+                transition={{ 
+                  type:"tween", 
+                  ease: "easeIn", 
+                  delay: 0.4 
+                }}
+              >
+                <div className="flex justify-center mt-2">
+                  <img className="flex justify-center opacity-90 w-28 mb-8" src={target} alt="arrow target">
+                  </img>
+                </div>
+                <h3 className="text-yellow-500 lg:text-2xl shadow-sm">GOALS</h3>
+                <p className="text-lg lg:text-xl mx-8">
+                  My goal is to work with a team to develop dynamic and responsive web applications that can make an impact in the tech industry. 
+                  I am committed to pursuing opportunities that challenge and push me to grow both personally and professionally as a web developer.
+                </p>
+              </motion.div>
+
+              <motion.div 
+                className="text-center mt-7 p-4"
+                animate={{ x: 0, opacity: 1 }} 
+                initial={{ x: '-100vw', opacity: 0 }} 
+                transition={{ type:"tween", ease: "easeIn", delay: 0.6 }}
+              >
+                <div className="flex justify-center mb-7 mt-2">
+                  <img className="opacity-90 w-28" src={briefcase} alt="briefcase">
+                    </img>
+                </div>
+                <h3 className="text-yellow-500 lg:text-2xl shadow-sm mt-6">EXPERIENCE</h3>
+                  <motion.p className="text-lg lg:text-xl mx-8">
+                      My background in customer service and team leadership roles has given me the ability to communicate effectively and work collaboratively. Having built and led a team of over 40 employees, I understand the importance of effective communication.
+                  </motion.p>
+              </motion.div>
+
+              <motion.div 
+                className="p-4"
+                animate={{ x: 0, opacity: 1 }} initial={{ x: '100vh', opacity: 0 }} 
+                transition={{ type:"tween", ease: "easeIn", delay: 0.8 }}
+              >
+                <div className="flex justify-center">
+                  <img className="flex justify-center opacity-90 mb-6 w-36 animate-pulse" src={lightbulb} alt="lightbulb">
+                    </img>
+                </div>
+                <h3 className="text-yellow-500 lg:text-2xl shadow-sm">TRAITS</h3>
+                  <motion.p className="text-lg lg:text-xl mx-8">
+                  As a diligent and courteous individual, I hold great regard for gaining knowledge from others. During my leisure time, I take pleasure in practicing woodworking and coding. I firmly believe that attentive listening is crucial for continuous personal and professional development.
+                  </motion.p>
+              </motion.div>
+
+            </div>
+            <motion.button 
+              className='text-lg lg:text-2xl animate-pulse p-0 mt-8 mb-10 text-amber-500 hover:text-white' 
+              onClick={() => handlePageChange('Skills')} 
+              animate={{ x: 0, opacity: 1 }} 
+              initial={{ x: '-100vh', opacity: 0 }} 
+              transition={{ type:'spring',delay: 2 }}
+            >
+              Continue &#x2192;
+            </motion.button>
           </div> 
     );
 };
--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,7 +1,80 @@
 import React from "react";
+import getFitPic from "../../assets/screenshots/getfitScreenshot.png";
+import bringPopcornPic from "../../assets/screenshots/bringpopcornscreenshot.png";
+import omegaBlogsPic from "../../assets/screenshots/dashboardScreenshot.png";
+import getFitLogo from "../../assets/screenshots/MuscleLogoNoCircle.png";
+import omegaBlogsLogo from "../../assets/screenshots/omegaLogo.png";
+import popcornLogo from "../../assets/screenshots/popcornLogo.png";
+import githubLogo from "../../assets//icons/GithubLogowhite.png"
 
 export default function Skills() {
     return (
-        <h1>Skills</h1>
+        <div>
+            <h1 className="text-center lg:text-6xl text-blue-400 mt-10">My Projects</h1>
+            <h6 className="text-center">Click to view projects</h6>
+
+            <div className="flex justify-center mt-10">
+               
+                <div>
+                    {/* The button to open modal */}
+                    <label htmlFor="omegaModal" className="btn bg-transparent border-none hover:bg-transparent">
+                        <img className=" w-24" alt="omega symbol" src={omegaBlogsLogo}></img>
+                    </label>
+
+                    {/* Put this part before </body> tag */}
+                    <input type="checkbox" id="omegaModal" className="modal-toggle" />
+                    <div className="modal">
+                        <div className="modal-box bg-neutral-500">
+                            <h3 className="text-6xl text-yellow-500 border-b-2 border-black">Omega Blogs</h3>
+                            <p className="py-4 text-xl font-bold text-black">Omega Blogs is a space in which you can share your ideas and gain knowledge from others.</p>
+                            <img src={omegaBlogsPic}></img>
+                            <div className="modal-action">
+                                <label htmlFor="omegaModal" className="btn bg-black">Close</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {/* The button to open modal */}
+                    <label htmlFor="getFitModal" className="btn bg-transparent border-none hover:bg-transparent">
+                        <img className=" w-32" src={getFitLogo}></img>
+                    </label>
+
+                    {/* Put this part before </body> tag */}
+                    <input type="checkbox" id="getFitModal" className="modal-toggle" />
+                    <div className="modal">
+                        <div className="modal-box bg-neutral-500">
+                            <h3 className="text-6xl text-yellow-500 border-b-2 border-black">GetFit</h3>
+                            <p className="py-4 text-xl font-bold text-black">GetFit is a website devoted to assisting users to become physically active and providing the necessary tools to aid users on their fitness journeys.</p>
+                            <img src={getFitPic}></img>
+                            <div className="modal-action">
+                                <label htmlFor="getFitModal" className="btn bg-black">Close</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {/* The button to open modal */}
+                    <label htmlFor="bringPopModal" className="btn bg-transparent border-none hover:bg-transparent">
+                        <img className=" w-20" alt="" src={popcornLogo}></img>
+                    </label>
+
+                    {/* Put this part before </body> tag */}
+                    <input type="checkbox" id="bringPopModal" className="modal-toggle" />
+                    <div className="modal">
+                        <div className="modal-box bg-neutral-500">
+                            <h3 className="text-6xl text-yellow-500 border-b-2 border-black">Bring Popcorn</h3>
+                            <p className="py-4 text-xl font-bold text-black">Bring Popcorn is a movie search website that was developed with the goal of providing users with one, convenient location to discover and search for movies.</p>
+                            <img src={bringPopcornPic}></img>
+                            <div className="modal-action">
+                                <label htmlFor="bringPopModal" className="btn bg-black">Close</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
     )
 };
